fbaf01fb6b42138007bc773c527a7f363c5264d1
Hesham Massoud <hesham.massoud@commercetools.de>
2017-10-27 15:25:55 +0200

    Add bash run-script

---
+++ run-script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+REGISTRY='heshamm'
+NAME='jenkins-job-prototype'
+TAG='production'
+IMAGE="${REGISTRY}/${NAME}:${TAG}"
+docker pull ${IMAGE}
+# ensure the exited container is removed (workaround for https://github.com/docker/docker/issues/20744)
+docker rm ${NAME} || true
+docker run 
